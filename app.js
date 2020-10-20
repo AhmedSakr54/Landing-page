8f399a0d7c85154e858e8d589bb4d79605bc319c
AhmedSakr54 <ahmedsakr3645@gmail.com>
2020-10-20 18:34:38 +0200

    Refactor some code in app.js

--- app.js
+++ app.js
@@ -1,35 +1,50 @@
-let slideIndex = 1;
 const nextBtn = document.querySelector(".next");
 const prevBtn = document.querySelector(".prev");
 const slides = document.querySelectorAll(".my-slide");
 const dots = document.querySelectorAll(".dot");
 
+let currentSlideIndex = 1;
+let prevSlideIndex = 0;
+/**
+ * Show the next slide in line to the client and hide all other slides
+ * @param {int} n 
+ */
 const showSlides = (n) => {
+    // when true that means the next or prev buttons were pressed 
+    // Then hide the previous slide
+    if (prevSlideIndex > 0) {
+        slides[prevSlideIndex - 1].className = slides[prevSlideIndex - 1].className.replace("show-slide", "hide-slide");
+        dots[prevSlideIndex - 1].className = dots[prevSlideIndex - 1].className.replace("active", "");
+    }
+    // when true that means its the end of the slides and we should reset to the first slide
     if (n > slides.length)
-        slideIndex = 1;
+        currentSlideIndex = 1;
+    // when true that means there is no previous slides and we should go to the last slide 
     if (n < 1)
-        slideIndex = slides.length;
+        currentSlideIndex = slides.length;
 
-    for (const slide of slides) {
-        slide.classList.remove("show-slide");
-        slide.classList.add("hide-slide");
-    }
-    for (const dot of dots) {
-        dot.className = dot.className.replace(" active", "");
-    }
-    slides[slideIndex - 1].classList.remove("hide-slide");
-    slides[slideIndex - 1].classList.add("show-slide");
-    dots[slideIndex - 1].classList.add("active");
+    // show the expected slide to the client
+    slides[currentSlideIndex - 1].className = slides[currentSlideIndex - 1].className.replace("hide-slide", "show-slide");
+    dots[currentSlideIndex - 1].classList.add("active");
+    prevSlideIndex = currentSlideIndex;
 };
 
+/**
+ * Change the currentSlideIndex to the next or prev slide depending on the button pressed (next/prev)
+ * @param {int} n 
+ */
 const changeSlide = (n) => {
-    slideIndex += n;
-    showSlides(slideIndex);
+    currentSlideIndex += n;
+    showSlides(currentSlideIndex);
 };
 
+/**
+ * Change the currentSlideIndex to the corresponding dot pressed
+ * @param {int} n 
+ */
 const currentSlide = (n) => {
-    slideIndex = n;
-    showSlides(slideIndex);
+    currentSlideIndex = n;
+    showSlides(currentSlideIndex);
 };
 
 const addListenersToSlideShowBtns = () => {
@@ -48,6 +63,16 @@ const addListenersToSlideShowBtns = () => {
     }
 };
 
+/**
+ * show the first slide in the html and hide the rest
+ */
+const showFirstSlideAndHideTheRest = () => {
+    slides[0].classList.add("show-slide");
+    for (let i = 1; i < slides.length; i++) {
+        slides[i].classList.add("hide-slide");
+    }
+};
 
+showFirstSlideAndHideTheRest();
 addListenersToSlideShowBtns();
-showSlides(slideIndex);
+showSlides(currentSlideIndex);
