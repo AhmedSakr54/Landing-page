27b7bf1c5c1dd310096a6bae52120e62560dc994
AhmedSakr54 <ahmedsakr3645@gmail.com>
2020-10-20 13:39:17 +0200

    Fix for warning in app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 let slideIndex = 1;
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const slides = document.querySelectorAll(".my-slide");
+const dots = document.querySelectorAll(".dot");
+
 const showSlides = (n) => {
-    const slides = document.getElementsByClassName("my-slide");
-    const dots = document.getElementsByClassName("dot");
     if (n > slides.length)
         slideIndex = 1;
     if (n < 1)
         slideIndex = slides.length;
-    
+
     for (const slide of slides) {
         slide.classList.remove("show-slide");
         slide.classList.add("hide-slide");
@@ -14,16 +17,37 @@ const showSlides = (n) => {
     for (const dot of dots) {
         dot.className = dot.className.replace(" active", "");
     }
-    slides[slideIndex-1].classList.remove("hide-slide");
-    slides[slideIndex-1].classList.add("show-slide");
-    dots[slideIndex-1].classList.add("active");
+    slides[slideIndex - 1].classList.remove("hide-slide");
+    slides[slideIndex - 1].classList.add("show-slide");
+    dots[slideIndex - 1].classList.add("active");
 };
-showSlides(slideIndex);
+
 const changeSlide = (n) => {
     slideIndex += n;
     showSlides(slideIndex);
 };
+
 const currentSlide = (n) => {
     slideIndex = n;
     showSlides(slideIndex);
 };
+
+const addListenersToSlideShowBtns = () => {
+
+    nextBtn.addEventListener("click", () => {
+        changeSlide(1);
+    });
+    prevBtn.addEventListener("click", () => {
+        changeSlide(-1);
+    });
+    for (let i = 0; i < dots.length; i++) {
+        const index = i + 1;
+        dots[i].addEventListener("click", () => {
+            currentSlide(index);
+        });
+    }
+};
+
+
+addListenersToSlideShowBtns();
+showSlides(slideIndex);
